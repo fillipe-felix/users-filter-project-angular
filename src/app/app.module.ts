a197cb8045e7199bc69439160f561bd7257f31d5
fillipe.felix <felipesoares_1993@hotmail.com>
2024-12-15 10:20:53 -0300

    76 - Adicionar layout flexbox e redefinições de estilo global
    Introduzido um novo layout de contêiner baseado em flexbox em `app.component.html` e estilos correspondentes em `app.component.scss`. Atualizado `styles.scss` com redefinições de estilo global e formatação aprimorada em `app.module.ts` para consistência.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ComponentsModule} from './components/components.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
